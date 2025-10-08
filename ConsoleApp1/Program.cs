f03b9211a9656c15dd4948016f6365f43d03adb2
maknaknets <abbasovdzamil@gmail.com>
2025-10-08 10:12:35 +0300

    функции над текстом

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -30,5 +30,34 @@ class Program
                 Console.WriteLine("Ошибка: введённый текст меньше 100 символов.");
             }
         }
+        if (statisticsList.Count > 1)
+        {
+            foreach (var stat in statisticsList)
+            {
+                Console.WriteLine("\nСтатистика предыдущего текста:");
+                DisplayStats(stat);
+            }
+        }
+    }
+        private static TextStatistics ProcessText(string text)
+        {
+            var words = SplitIntoWords(text);                  // Разбор на слова
+            int wordCount = CountWords(words);                 // Подсчёт количества слов
+            string shortestWord = FindShortestWord(words);     // Поиск короткого слова
+            int sentenceCount = CountSentences(text);          // Счёт предложений
+            int vowelsCount = CountVowels(text);               // Количество гласных
+            int consonantsCount = CountConsonants(text);       // Количество согласных
+            string longestWord = FindLongestWord(words);       // Самое длинное слово
+            Dictionary<char, int> letterFrequency = CalculateLetterFrequency(text); // Частота букв
+
+            return new TextStatistics(wordCount, shortestWord, sentenceCount,
+                                      vowelsCount, consonantsCount, longestWord, letterFrequency);
+            }
+        private static string[] SplitIntoWords(string text)
+        {
+            char[] separators = { ' ', '\t', '\n' };
+            return text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
     }
 }
