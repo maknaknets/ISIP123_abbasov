c3f22f90f124894735522a49d2a88e4a1448964a
maknaknets <abbasovdzamil@gmail.com>
2025-10-08 14:53:19 +0300

    ищщл сдфыы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace LibraryManagement
+{
+    // Enum for genres
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Mystery,
+        ScienceFiction,
+        Biography
+    }
+
+    // Book class
+    public class Book
+    {
+        private static int nextId = 1;
+
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string title, string author, Genre genre, int year, decimal price)
+        {
+            Id = nextId++;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Year = year;
+            Price = price;
+        }
