5737864e961fc9e25519714a1a0956ba2e25ab3c
maknaknets <abbasovdzamil@gmail.com>
2025-10-13 12:58:33 +0300

    инкапсусляция

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,30 @@
-﻿ssss
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Абстрактный класс Person (Наследование и Абстракция)
+public abstract class Person
+{
+    // Инкапсуляция
+    private string id;
+    private string name;
+    private int age;
+    private string contactInfo;
+
+    public Person(string id, string name, int age, string contactInfo)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.contactInfo = contactInfo;
+    }
+    // Свойства (Инкапсуляция)
+    public string Id => id;
+    public string Name => name;
+    public int Age => age;
+    public string ContactInfo => contactInfo;
+
+    // Абстрактный метод для полиморфизма
+    public abstract string GetDetails();
+}
